89e19ad9c628543e695a67541f0b571c75e4c4dd
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-01-17 14:26:34 +0100

    Added setup shell script.

---
+++ setup.sh
@@ -0,0 +1,3 @@
+sudo apt install build-essential cmake python-dev python3-dev mono-xbuild libmono-system-xml-linq4.0-cil libmono-microsoft-csharp4.0-cil libmono-system-data-datasetextensions4.0-cil
+cd bundle/YouCompleteMe
+./install.py --all
