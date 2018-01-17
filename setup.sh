39c66d2f85216d78d4dbc5e5aa33e51f12a5368a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2018-01-17 14:46:57 +0100

    Fixed setup.sh file.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+git submodule update --init --recursive
 sudo apt install build-essential cmake python-dev python3-dev mono-xbuild libmono-system-xml-linq4.0-cil libmono-microsoft-csharp4.0-cil libmono-system-data-datasetextensions4.0-cil
 cd bundle/YouCompleteMe
 ./install.py --all
