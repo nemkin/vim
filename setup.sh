5ac9cf8d2d36196a4ccda3e1058dc945ce8afc02
Viktória Nemkin <viktoria.nemkin@gmail.com>
2020-07-23 13:22:10 +0200

    Fix sending error to void

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cd bundle/YouCompleteMe
 ./install.py --clang-completer
 cd -
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-rm ~/.vimrc > /dev/null
+rm ~/.vimrc &> /dev/null
 ln -s $DIR/vimrc ~/.vimrc
