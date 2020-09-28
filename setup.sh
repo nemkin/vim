1eb380e333488e17bcfb7cd708aece55a24d5996
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2020-09-28 10:01:40 +0200

    Add screenrc, fix install

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 git submodule update --init --recursive
 sudo apt install build-essential cmake python-dev python3-dev
 cd bundle/YouCompleteMe
-./install.py --clang-completer --system-libclang
+./install.py # --clang-completer --system-libclang
 cd -
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm ~/.vimrc &> /dev/null
 ln -s $DIR/vimrc ~/.vimrc
+ln -s $DIR/screenrc ~/.screenrc
