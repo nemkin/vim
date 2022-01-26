492bc223cd8f0a552332b30ee3424fa907871457
Viktória Nemkin <viktoria.nemkin@gmail.com>
2022-01-26 23:30:31 +0000

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 git submodule update --init --recursive
-sudo apt install build-essential cmake python-dev python3-dev
-cd bundle/YouCompleteMe
-./install.py # --clang-completer --system-libclang
-cd -
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(dirname "$0")"
 rm ~/.vimrc &> /dev/null
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/screenrc ~/.screenrc
