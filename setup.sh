7690831d4966a400cd5b66d2b7fafa80764f230a
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2025-05-19 09:24:15 +0200

    Fix vim

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 git submodule update --init --recursive
-DIR="$(dirname "$0")"
+DIR="$(cd "$(dirname "$0")" && pwd)"
 rm ~/.vimrc &> /dev/null
+rm ~/.screenrc &> /dev/null
 ln -s $DIR/vimrc ~/.vimrc
 ln -s $DIR/screenrc ~/.screenrc
